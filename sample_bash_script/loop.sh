034bfecc90da96df15fa4cf24c23ccfa99d938d8
Jaimee-Ian Rodriguez <jaimeeian@CEN-0033605.local>
2017-05-30 14:17:13 -0400

    Added my first bash loop

---
+++ sample_bash_script/loop.sh
@@ -0,0 +1,11 @@
+# Comments are preceded by the hash symbol. You can use the comments as documenttion, or just to temporarily remove the lines from being executed.
+
+# This is a sample loop in bash.
+
+for i in {1..10};
+do echo $i;
+for j in {a..g};
+do echo $j;
+done
+done
+
