738c9926430ada2318c97118339d32e01cd1bc5c
Jaimee-Ian <Jaimee-Ian.rodriguez@macaulay.cuny.edu>
2017-05-31 11:23:31 -0400

    We added a new python script. For now it just contains initial documentation.

---
+++ pythonpractice/practice_script.py
@@ -0,0 +1,3 @@
+'''
+This script contains an exampleclass that we will write. The class will be called algebra and it will contain some simple methods on how to do osome algebraic manipulation. The script will end with a sample way to call and use the class.
+'''
